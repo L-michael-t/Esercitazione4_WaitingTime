5e8a5e1988eacde23b0c4ba31d543f7ae475847e
gabriele ranieri <ranigabri07@gmail.com>
2026-01-20 13:49:55 +0100

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,15 @@
-package org.example;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public void main(String[] args) {
+    try (BufferedReader br = new BufferedReader(new FileReader("music.json"))) {
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
         }
+    } catch (IOException e) {
+        System.out.println("errore nel leggere il file");
     }
 }
+
